ce064fb5546563debd0a929d477bb8f56c186b92
CedricM9 <66473994+CedricM9@users.noreply.github.com>
2020-11-30 16:25:54 +0100

    Update marching_cubes_reconstructor.h

--- LabProject/SurfaceReconstruction/alg/marching_cubes_reconstructor.h
+++ LabProject/SurfaceReconstruction/alg/marching_cubes_reconstructor.h
@@ -7,15 +7,39 @@
 #include "possibleGeometry.data"
 #include "edgeIdentification.data"
 
+/** \class marchingCubesReconstructor
+ * \brief Class for reconstructing the surface mesh
+ * \details This class fully reconstructs the surface mesh with the given parameters.
+ * It calls all required subsequent functions
+ */
 class marchingCubesReconstructor : public surfaceReconstructor {
   public:
     marchingCubesReconstructor() {}
 
+    /** \brief Calculates the particle densitiy of each particle of the simulation
+     * \details This function determines how many particles surround a specified particle
+     * and returns a value to represent this density
+     * \param nearestNeighbors Contains the neighbors of each particle
+     * \param kernelPointer Pointer to the SPH Interpolation Kernel
+     * \param particles List of all particles
+     * \param h Also referred to as smoothing lenght. Required by the SPH interpolation
+     */
     void calculateDensities(std::vector<std::vector<unsigned int>>& nearestNeighbors, 
                             std::shared_ptr<SPHInterpolationKernel> kernelPointer,
                             particleList& particles,
                             float h);
-
+  
+    /** \brief Generates entire surface mesh
+     * \details This function takes care of the entire reconstruction process for a single frame
+     * \param g Graph which has been created for the simulation data
+     * \param particles List of all particles
+     * \param smoothingLength Also referred to as h. Required by the SPH Interpolation
+     * \param compactSupport The radius for the neighborhood search. Should be 2*h
+     * \param iso Value to exclude outlying particles
+     * \param levelSetPointer Pointer to the level set function
+     * \param nSearchPointer Pointer to the compact support neighborhood search
+     * \param kernelPointer Pointer to the specified SPH Interpolation Kernel
+     */
     virtual triangleList reconstruct(graph& g,
                              particleList& particles,
                              const float smoothingLength,
